95db2564f88f60ce12d710bac4c48e1b2151e3d4
Ashley Ye <ashley.z.ye@gmail.com>
2021-01-16 22:55:06 -0500

    final version of first site

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <React.Fragment>
+      
+      <header id="home">
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+             <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
+            </ul>
+         </nav>
+         
+         <div className="row banner">  
+            <div className="banner-text">
+                <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+                <h1 className="responsive-headline">{resumeData.name}</h1>
+                <h3 style={{color:'#fff', fontFamily:'sans-serif', fontSize: '20px'}}>Hi! I'm a {resumeData.role}.{resumeData.roleDescription}
+                </h3>
+                <h3 style={{color:'#fff', fontFamily:'sans-serif', fontSize: '20px'}}>Feel free to check out my <a className="smoothscroll" href="#portfolio">projects</a> below, contact me at <a href="mailto:ashleyye@berkeley.edu" target="_">ashleyye@berkeley.edu</a> or check out my <a href="assets/AshleyYe-Resume.pdf" target="_">resume</a>!</h3>
+                <hr/>
+                <ul className="social">
+                    {
+                        resumeData.socialLinks && resumeData.socialLinks.map(item =>{
+                        return(
+                                <li key={item.name}>
+                                    <a href={item.url} target="_blank"><i className={item.className}></i></a>
+                                </li>
+                                )
+                            }
+                        )
+                    }
+               </ul>
+            </div>
+         </div>
+
+         <p className="scrolldown">
+            <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
+         </p>
+
+      </header>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,128 @@
+let resumeData = {
+    "imagebaseurl":"https://ivy555.github.io/",
+    "name": "Ashley Ye",
+    "role": "student studying Electrical Engineering and Computer Science at UC Berkeley",
+    "linkedinId":"ashlye",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I'm interested in machine learning, web development, and software engineering, but I'm open to all opportunities.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ashlye",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ashleyye",
+          "className":"fa fa-github"
+        },
+        {
+            "name":"email",
+            "url":"mailto:ashleyye@berkeley.edu",
+            "className":"fa fa-envelope"
+          },
+        {
+            "name":"resume",
+            "url":"assets/AshleyYe-Resume",
+            "className":"fa fa-paperclip"
+          },
+      ],
+    "aboutme":"I am currently a Electrical Engineering and Computer Science major at UC Berkeley. other infooooJSKDFJKDSLJDK",
+    "address":"240-888-0365",
+    "website":"ashleyye@berkeley.edu",
+    "education":[
+      {
+        "UniversityName":"University of California, Berkeley",
+        "specialization":"Electrical Engineering and Computer Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2023",
+        "Achievements":"Coursework: Data Structures, Discrete Mathematics and Probability Theory, Structure and Interpretation of Computer Programs, Designing Information Devices and Systems I, Multivariable Calculus"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"NASA Goddard Space Flight Center",
+        "specialization":"Business Marketing Intern",
+        "MonthOfStarting":"Jun",
+        "YearOfStarting":"2018",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019",
+        "Achievements":"Raised public awareness for the concept and importance of NASA spinoffs through user-friendly visuals with the Strategic Partnerships Office."
+      },
+      {
+        "CompanyName":"The Farside Review",
+        "specialization":"Web Development Manager",
+        "MonthOfStarting":"Dec",
+        "YearOfStarting":"2020",
+        "MonthOfLeaving":"Present",
+        "Achievements":"Maintains website and submission portal for the online quarterly literary magazine."
+      },
+      {
+        "CompanyName":"Berkeley PlexTech",
+        "specialization":"Web Development Curriculum Student",
+        "MonthOfStarting":"Sept",
+        "YearOfStarting":"2020",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2020",
+        "Achievements":"Learned about web development fundamentals other web development through lectures and hands-on projects."
+      },
+      {
+        "CompanyName":"Berkeley IEEE",
+        "specialization":"Technical Operations Officer",
+        "MonthOfStarting":"Sept",
+        "YearOfStarting":"2020",
+        "MonthOfLeaving":"Present",
+        "Achievements":"Maintains website and submission manager for the quarterly online literary magazine."
+      },
+    ],
+    /*"skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],*/
+    "portfolio":[
+      {
+        "name":"equiScan",
+        "description":"Web-Based Answer Sheet Scanner",
+        "imgurl":"images/portfolio/equiscan.jpg",
+        "projurl":"https://github.com/ashleyye/plexhacks"
+      },
+      {
+        "name":"Autonomous Driving via Accident Analytics",
+        "description":"Virtual Self-Driving Car",
+        "imgurl":"images/portfolio/autonomousdriving.jpg",
+        "projurl":"https://devpost.com/software/equiscan"
+      },
+      {
+        "name":"Proapptive",
+        "description":"Web-Based Productivity App",  
+        "imgurl":"images/portfolio/proapptive.png",
+        "projurl":"https://github.com/srikarmunukutla/Productivity-App"
+      },
+      {
+        "name":"Sudoku Solver",
+        "description":"Repeated Algorithm Approach Sudoku Solver",
+        "imgurl":"images/portfolio/sudokusolver.png",
+        "projurl":"https://github.com/ashleyye/sudokusolver"
+      }
+    ],
+    /*"testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]*/
+  }
+  
+  export default resumeData
